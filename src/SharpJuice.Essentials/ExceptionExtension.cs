d94042e8a379c2a686bc3272479d56a640804e00
Alec Anikin <alec.anikin@gmail.com>
2018-06-25 17:56:07 +0300

    Rename ThrowIfEmpty to OrThrow and return value instead of Maybe

--- src/SharpJuice.Essentials/ExceptionExtension.cs
+++ src/SharpJuice.Essentials/ExceptionExtension.cs
@@ -1,58 +1,40 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SharpJuice.Essentials
 {
     public static class ExceptionExtension
     {
-        public static IEnumerable<T> ThrowIfEmpty<T>(this IEnumerable<T> items, Exception ex)
-        {
-            if (!items.Any())
-                throw ex;
-
-            return items;
-        }
-
-        public static IEnumerable<T> ThrowIfEmpty<T>(this IEnumerable<T> items, Func<Exception> exceptionFactory)
-        {
-            if (!items.Any())
-                throw exceptionFactory();
-
-            return items;
-        }
-
-        public static Maybe<T> ThrowIfEmpty<T>(this Maybe<T> maybe, Exception ex)
+        public static T OrThrow<T>(this Maybe<T> maybe, Exception ex)
         {
             if (!maybe.Any())
                 throw ex;
 
-            return maybe;
+            return maybe.Single();
         }
 
-        public static Maybe<T> ThrowIfEmpty<T>(this Maybe<T> maybe, Func<Exception> exceptionFactory)
+        public static T OrThrow<T>(this Maybe<T> maybe, Func<Exception> exceptionFactory)
         {
             if (!maybe.Any())
                 throw exceptionFactory();
 
-            return maybe;
+            return maybe.Single();
         }
 
-        public static async Task<Maybe<T>> ThrowIfEmpty<T>(this Task<Maybe<T>> maybeTask, Exception ex)
+        public static async Task<T> OrThrow<T>(this Task<Maybe<T>> maybeTask, Exception ex)
         {
             if (!(await maybeTask).Any())
                 throw ex;
 
-            return maybeTask.Result;
+            return maybeTask.Result.Single();
         }
 
-        public static async Task<Maybe<T>> ThrowIfEmpty<T>(this Task<Maybe<T>> maybeTask, Func<Exception> exceptionFactory)
+        public static async Task<T> OrThrow<T>(this Task<Maybe<T>> maybeTask, Func<Exception> exceptionFactory)
         {
             if (!(await maybeTask).Any())
                 throw exceptionFactory();
 
-            return maybeTask.Result;
+            return maybeTask.Result.Single();
         }
 
         //The reason of the methods BindThrow* below is incorrect Bind overload choosing for lambda i => throw new Ex()
