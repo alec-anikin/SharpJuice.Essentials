25e3bfa5604c98ac73d095a716275d4cbd9c0b88
Alec Anikin <alec.anikin@gmail.com>
2018-06-26 10:31:39 +0300

    BindThrow* return void

--- src/SharpJuice.Essentials/ExceptionExtension.cs
+++ src/SharpJuice.Essentials/ExceptionExtension.cs
@@ -40,36 +40,28 @@ namespace SharpJuice.Essentials
         //The reason of the methods BindThrow* below is incorrect Bind overload choosing for lambda i => throw new Ex()
         //https://stackoverflow.com/questions/23486452/lambda-of-x-throw-inferred-to-match-funct-task-in-overloaded-metho
 
-        public static Maybe<T> BindThrow<T>(this Maybe<T> maybe, Exception ex)
+        public static void BindThrow<T>(this Maybe<T> maybe, Exception ex)
         {
             if (maybe.Any())
                 throw ex;
-
-            return maybe;
         }
 
-        public static Maybe<T> BindThrow<T>(this Maybe<T> maybe, Func<Exception> exceptionFactory)
+        public static void BindThrow<T>(this Maybe<T> maybe, Func<Exception> exceptionFactory)
         {
             if (maybe.Any())
                 throw exceptionFactory();
-
-            return maybe;
         }
 
-        public static async Task<Maybe<T>> BindThrow<T>(this Task<Maybe<T>> maybeTask, Exception ex)
+        public static async Task BindThrow<T>(this Task<Maybe<T>> maybeTask, Exception ex)
         {
             if ((await maybeTask).Any())
                 throw ex;
-
-            return maybeTask.Result;
         }
 
-        public static async Task<Maybe<T>> BindThrow<T>(this Task<Maybe<T>> maybeTask, Func<Exception> exceptionFactory)
+        public static async Task BindThrow<T>(this Task<Maybe<T>> maybeTask, Func<Exception> exceptionFactory)
         {
             if ((await maybeTask).Any())
                 throw exceptionFactory();
-
-            return maybeTask.Result;
         }
     }
 }
