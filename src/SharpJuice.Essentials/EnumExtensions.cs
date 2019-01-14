bcad625b007ca37e7166cf86bf28cfc6642ba51e
Alec Anikin <alec.anikin@gmail.com>
2019-01-14 23:27:42 +0300

    Fix enum parsing case ignoring

--- src/SharpJuice.Essentials/EnumExtensions.cs
+++ src/SharpJuice.Essentials/EnumExtensions.cs
@@ -6,7 +6,7 @@ namespace SharpJuice.Essentials
     {
         public static Maybe<TEnum> ToEnum<TEnum>(this string value, bool ignoreCase = false) where TEnum : struct
         {
-            return Enum.TryParse(value, out TEnum result)
+            return Enum.TryParse(value, ignoreCase, out TEnum result)
                 ? result
                 : new Maybe<TEnum>();
         }
