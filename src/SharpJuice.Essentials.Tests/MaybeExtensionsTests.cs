d708954306a8978a4eadc8b3a175af9b715a6357
Anikin Aleksandr <aanikin@ozon.ru>
2021-11-25 12:42:27 +0300

    Allocation free SelectMany + github actions

---
+++ src/SharpJuice.Essentials.Tests/MaybeExtensionsTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using SharpJuice.Essentials;
+using Xunit;
+
+namespace MaybeLinq.Tests
+{
+    public sealed class MaybeExtensionsTests
+    {
+        [Fact]
+        public void Primitive_EmptySource_EmptyResult()
+        {
+            var collection = Array.Empty<Maybe<int>>();
+
+            var results = collection.SelectMany(i => i);
+
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Primitive_SourceContainsOnlyEmptyValues_EmptyResult()
+        {
+            var collection = new[]
+            {
+                new Maybe<int>(),
+                new Maybe<int>(),
+                new Maybe<int>()
+            };
+
+            var results = collection.SelectMany(i => i);
+
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Primitive_SourceContainsValues_AllValuesReturned()
+        {
+            var collection = new Maybe<int>[] { 1, 2, 3 };
+
+            var results = collection.SelectMany(i => i);
+
+            results.Should().BeEquivalentTo(new[] { 1, 2, 3 });
+        }
+
+        [Fact]
+        public void Primitive_SourceContainsEmptyValues_NotEmptyValuesReturned()
+        {
+            var collection = new Maybe<int>[] { 1, new(), 3 };
+
+            var results = collection.SelectMany(i => i);
+
+            results.Should().BeEquivalentTo(new[] { 1, 3 });
+        }
+
+        [Fact]
+        public void Complex_EmptySource_EmptyResult()
+        {
+            var collection = Array.Empty<Complex>();
+
+            var results = collection.SelectMany(i => i.Value);
+
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Complex_SourceContainsOnlyEmptyValues_EmptyResult()
+        {
+            var collection = new Complex[]
+            {
+                new(new Maybe<int>()),
+                new(new Maybe<int>()),
+                new(new Maybe<int>())
+            };
+
+            var results = collection.SelectMany(i => i.Value);
+
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Complex_SourceContainsValues_AllValuesReturned()
+        {
+            var collection = new Complex[] { new(1), new(2), new(3) };
+
+            var results = collection.SelectMany(i => i.Value);
+
+            results.Should().BeEquivalentTo(new[] { 1, 2, 3 });
+        }
+
+        [Fact]
+        public void Complex_SourceContainsEmptyValues_NotEmptyValuesReturned()
+        {
+            var collection = new Complex[] { new(1), new(new Maybe<int>()), new(3) };
+
+            var results = collection.SelectMany(i => i.Value);
+
+            results.Should().BeEquivalentTo(new[] { 1, 3 });
+        }
+
+        private sealed class Complex
+        {
+            public Complex(Maybe<int> value)
+                => Value = value;
+
+            public Maybe<int> Value { get; }
+        }
+    }
+}
---
+++ src/SharpJuice.Essentials/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SharpJuice.Essentials;
+
+namespace System.Linq
+{
+    public static class MaybeEnumerable
+    {
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, Maybe<TResult>> selector)
+        {
+            using var enumerator = source.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var maybe = selector(enumerator.Current);
+
+                if (maybe.Any())
+                    yield return maybe.Single();
+            }
+        }
+    }
+}
